6069ddde6ca50820476887a69adb51217f7c1920
fmway <fm18lv@gmail.com>
2025-08-04 17:49:17 +0700

    chore(util/genModules): no pipe pipe

--- lib/fmway/__util/for-flake.nix
+++ lib/fmway/__util/for-flake.nix
@@ -1,80 +1,66 @@
 { lib, self', ... }: let
-  inherit (self'.fmway)
-    toCamelCase
-    withImport'
-    treeImport
-  ;
-in {
+
   genModules' = shareds: moduleDir: args: let
     modulesPath = builtins.toPath moduleDir;
-    re = lib.pipe modulesPath [
-      builtins.readDir
-      (lib.filterAttrs (_: v: v == "directory"))
-      (lib.attrNames)
-      (map (dir: let
-        scope = "${toCamelCase dir}Modules";
-      in {
-        name = scope;
-        value = let
-          res = args: lib.pipe "${modulesPath}/${dir}" [
-            (builtins.readDir)
-            (lib.filterAttrs (name: type:
-              (! isNull (builtins.match ".+[.]nix" name) && type == "regular") ||
-              (
-                type == "directory" &&
-                (
-                  lib.hasSuffix "-" name ||
-                  lib.pathIsRegularFile "${modulesPath}/${dir}/${name}/default.nix"
-                )
-              )
-            ))
-            (lib.attrNames)
-            (map (name: let
-              path = /. + "${modulesPath}/${dir}/${name}";
-              isDirectory = lib.pathIsDirectory path;
-              _file = path + lib.optionalString isDirectory "/default.nix";
-              isTree = isDirectory && lib.hasSuffix "-" path;
-              module =
-                let
-                  r = lib.removeSuffix ".nix" name;
-                  rr= lib.removeSuffix "-" name;
-                in if isTree then rr else if !isDirectory then r else name;
-            in {
-              name = module;
-              value = let
-                r = exc: let
-                  args' = lib.optionalAttrs (scope != "SharedModules") {
-                    allModules = map (x: final.${scope}.${x}) (
-                      lib.filter (x:
-                        x != module &&
-                        lib.all (y: x != y) (exc ++ [ "defaultWithout" "default" "all" "allWithout" ])
-                      ) (lib.attrNames final.${scope}));
-                  } // { inherit _file; } // args;
-                in if isTree then { config, pkgs ? {}, lib, osConfig ? {}, specialArgs ? {}, ... } @ v: treeImport { _file = path; } {
-                  folder = path;
-                  depth = 0;
-                  variables = args' // v // specialArgs;
-                } else withImport' _file args';
-              in if module == "default" then r else r [];
-            }))
-            (lib.listToAttrs)
-          ];
-        in if scope == "SharedModules" then
-          res
-        else res (final // args);
-      }))
-      (lib.listToAttrs)
-    ];
-    gen = lib.listToAttrs (map (name: {
+    listDir = attrNames (filterAttrs (_: v: v == "directory") (builtins.readDir modulesPath));
+    re = listToAttrs (map (x: let
+      scope = "${toCamelCase x}Modules";
+      dir = "${modulesPath}/${x}";
+    in {
+      name = scope;
+      value = let
+        filterModule = attrNames (filterAttrs (name: type:
+          (! isNull (builtins.match ".+[.]nix" name) && type == "regular") ||
+          (
+            type == "directory" &&
+            (
+              hasSuffix "-" name ||
+              pathIsRegularFile "${dir}/${name}/default.nix"
+            )
+          )
+        ) (builtins.readDir dir));
+        res = args: listToAttrs (map (name: let
+          path = /. + "${dir}/${name}";
+          isDirectory = pathIsDirectory path;
+          _file = path + optionalString isDirectory "/default.nix";
+          isTree = isDirectory && hasSuffix "-" path;
+          module =
+            let
+              r = removeSuffix ".nix" name;
+              rr= removeSuffix "-" name;
+            in if isTree then rr else if !isDirectory then r else name;
+        in {
+          name = module;
+          value = let
+            r = exc: let
+              args' = optionalAttrs (scope != "SharedModules") {
+                allModules = map (x: final.${scope}.${x}) (
+                  filter (x:
+                    x != module &&
+                    all (y: x != y) (exc ++ [ "defaultWithout" "default" "all" "allWithout" ])
+                  ) (attrNames final.${scope}));
+              } // { inherit _file; } // args;
+            in if isTree then { config, pkgs ? {}, lib, osConfig ? {}, specialArgs ? {}, ... } @ v: treeImport { _file = path; } {
+              folder = path;
+              depth = 0;
+              variables = args' // v // specialArgs;
+            } else withImport' _file args';
+          in if module == "default" then r else r [];
+        }) filterModule);
+      in if scope == "SharedModules" then
+        res
+      else res (final // args);
+    }) listDir);
+    gen = listToAttrs (map (name: {
       inherit name;
       value = re.SharedModules (final // args // { inherit name; }) // (re.${name} or {});
     }) shareds);
     final = let
-      r = removeAttrs re [ "SharedModules" ] // lib.optionalAttrs (re ? SharedModules) gen;
-    in lib.mapAttrs (k: v: v // {
-      allWithout = exc: { imports = map (x: final.${k}.${x}) (lib.filter (x: lib.all (y: x != y) exc) (lib.attrNames v)); };
+      r = removeAttrs re [ "SharedModules" ] // optionalAttrs (re ? SharedModules) gen;
+    in mapAttrs (k: v: v // {
+      allWithout = exc: { imports = map (x: final.${k}.${x}) (filter (x: all (y: x != y) exc) (attrNames v)); };
       all = final.${k}.allWithout [];
-    } // lib.optionalAttrs (v ? default) {
+    } // optionalAttrs (v ? default) {
       defaultWithout = v.default;
       default = final.${k}.defaultWithout [];
     }) r // {
@@ -82,5 +68,27 @@ in {
     };
   in final;
 
-  genModules = self'.fmway.genModules' [ "nixosModules" "nixDarwinModules" "homeManagerModules" ];
+  inherit (lib)
+    mapAttrs
+    all
+    listToAttrs
+    filter
+    attrNames
+    optionalAttrs
+    optionalString
+    removeSuffix
+    hasSuffix
+    filterAttrs
+    pathIsRegularFile
+    pathIsDirectory
+  ;
+  
+  inherit (self'.fmway)
+    toCamelCase
+    withImport'
+    treeImport
+  ;
+in {
+  inherit genModules';
+  genModules = genModules' [ "nixosModules" "nixDarwinModules" "homeManagerModules" ];
 }
