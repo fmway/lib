5f10f62c04877228e7c04ec630f8297ab28b019b
fmway <fm18lv@gmail.com>
2025-08-06 15:12:36 +0700

    chore(kdl): add shortand

--- lib/default.nix
+++ lib/default.nix
@@ -1,5 +1,7 @@
 { sources, lib, ... }: {
   readTree = import sources.read-tree {};
   mapListToAttrs = fn: l: lib.listToAttrs (map fn l);
-  kdl = import sources.kdl { inherit lib; };
+  kdl = let r = import sources.kdl { inherit lib; }; in r // { shorts = {
+    f = r.flag; l = r.leaf; l' = r.magic-leaf; n = r.node; p = r.plain; s = r.serialize;
+  }; };
 }
