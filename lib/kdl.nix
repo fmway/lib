2d1e35f8dcfe276fc6f1402a223f9a2323c90d27
fmway <fm18lv@gmail.com>
2025-08-10 23:20:12 +0700

    chore(kdl): add merge & assign method, works for all (i tought), except leaf

--- lib/kdl.nix
+++ lib/kdl.nix
@@ -19,18 +19,27 @@
             children,
           }:
     ''
+    ''
+      serialize.nodes-with =
+          indent:
+          lib.flip lib.pipe [
+    ''
   ] [
     "_A-Z" # add _ to regex
+    # nix
     ''
-      node = name: args: {
+      node = name: args: let res = {
           children = [];
           inherit name;
+          merge = args: res args // { _do = "merge"; };
+          assign = args: res args // { _do = "assign"; };
           inherit (fold-args (lib.toList args)) arguments properties;
-          __functor = self: args: self // {
+          __functor = self: args: removeAttrs self [ "merge" "assign" ] // {
             children = self.children ++ (if builtins.isList args then args else [args]);
           };
-        };
+        }; in res;
     ''
+    # nix
     ''
       leaf = name: args: node name args [ ] // rec {
           __functor = self: args: let
@@ -42,6 +51,7 @@
           };
         };
     ''
+    # nix
     ''
       serialize.node-with =
           indent:
@@ -52,6 +62,34 @@
             children,
             ...
           }:
+    ''
+    # nix
+    ''
+      serialize.nodes-with =
+          indent:
+          lib.flip lib.pipe [
+            # FIXME too complicated
+            (builtins.foldl' (acc: curr:
+              if ! curr ? _do then
+                acc ++ [curr]
+              else let
+                res = builtins.foldl' (a: c:
+                  a // (if c.name == curr.name && c.arguments == curr.arguments then
+                  lib.throwIf a.is_found "found duplicated nodes ''${curr.name}, i can't do ''${curr._do} twice" {
+                    is_found = true;
+                    data = let
+                      r = if curr._do == "merge" then
+                        c // {
+                          children = c.children ++ curr.children;
+                          properties = c.properties // curr.properties;
+                        }
+                      else if curr._do == "assign" then curr
+                      else throw "(kdl:serialize): i don't know what do you mean with ''${curr._do}";
+                    in a.data ++ [r];
+                  } else { data = a.data ++ [c]; })
+                ) { is_found = false; data = []; } acc;
+              in res.data ++ lib.optionals (!res.is_found) [curr]
+            ) [])
     ''
   ] (builtins.readFile sources.kdl));
   r = import patched { inherit lib; };
