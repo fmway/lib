7f6bf2bd789bb5688e43356e164d1dc5a59f32cf
fmway <fm18lv@gmail.com>
2026-01-19 21:05:49 +0700

    feat(each[,Default]System): init

--- lib/default.nix
+++ lib/default.nix
@@ -1,7 +1,16 @@
-{ sources, lib, self', ... }:
-{
+{ sources, lib, self', ... }: let
+  systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+in {
   readTree = import sources.read-tree {};
   mapListToAttrs = fn: l: lib.listToAttrs (map fn l);
   lexer  = import "${sources.nix-parsec}/lexer.nix" { parsec = self'.parsec; };
   parsec = import "${sources.nix-parsec}/parsec.nix";
+  eachSystem = systems: fn: builtins.foldl' (acc: system: let
+    r = fn system;
+  in self'.fmway.foldAttrs' (a: k: v: a // {
+    ${k} = a.${k} or {} // {
+      ${system} = v;
+    };
+  }) acc r) {} systems;
+  eachDefaultSystem = self'.eachSystem systems;
 }
