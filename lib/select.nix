a4aa698020cfd87b07569a18b24f9442b0d1adec
fmway <fm18lv@gmail.com>
2026-09-22 12:10:23 +0700

    lib.select: add {key:rename} / {?key:rename}

--- lib/select.nix
+++ lib/select.nix
@@ -1,22 +1,44 @@
 { sources, ... }: let
   # nix-select with .**. support => auto flatten attrValues / list
   patched = builtins.toFile "select.nix" (builtins.replaceStrings [
+    "acc_str = \"\";"
     "  if (mode == \"str\") || (mode == \"maybe\") then"
     "type = mode;"
+    "if (state.submode == \"\") && (cur == \"?\")"
     "else if (mode == \"str\") || (mode == \"maybe\") then"
+    "type = if state.submode == \"\" then \"str\" else state.submode;"
+    "{\n                acc_str = \"\${state.acc_str}\${cur}\";\n                submode = if state.submode == \"\" then \"str\" else state.submode;\n              }"
     "else if mode == \"start\" then\n          if cur == \"*\" then\n            recurse str (idx + 1) (\n              state\n              // {\n                stack = [ \"end\" ] ++ state.stack;\n                selectors = state.selectors ++ [ { type = \"all\"; } ];\n                acc_str = \"\";\n              }\n            )"
-    "selectors: obj:\n    let"
+    "applySelectors =\n    selectors: obj:\n    let"
     "selector = builtins.elemAt selectors idx;"
     "if builtins.isList obj then\n            if selector.type == \"all\" then"
     "if builtins.isAttrs obj then\n            if selector.type == \"all\" then\n              builtins.mapAttrs (_: v: recurse selectors (idx + 1) v) obj"
     "if x.type == \"maybe\" then"
     "if selector.type == \"maybe\" then"
+    "name = x.value;"
     "{ \${selector.value} = recurse selectors (idx + 1) (builtins.getAttr selector.value obj); }"
+    "\${sel.value}"
   ] [
+    # nix
+    ''
+      acc_str = "";
+      acc_rename = "";
+    ''
     # nix
     "  if (mode == \"str\") || (mode == \"maybe\") || (mode == \"maybe_\") || (mode == \"all\") || (mode == \"all_\") then"
     # nix
     "type = if mode == \"all_\" then \"all\" else mode;"
+    # nix
+    ''
+      if (state.submode != "maybe_") && (cur == ":") then
+        recurse str (idx + 1) (
+          state
+          // {
+            submode = if state.submode == "maybe" then "maybe_renamed" else "renamed";
+          }
+        )
+      else if (state.submode == "") && (cur == "?")
+    ''
     # nix
     ''
       else if mode == "maybe" && cur == "?" then
@@ -28,6 +50,20 @@
           }
         )
       else if (mode == "str") || (mode == "maybe") || (mode == "maybe_") || (mode == "all_") then
+    ''
+    # nix
+    ''
+      type = if state.submode == "" || state.submode == "renamed" then "str" else if state.submode == "maybe_renamed" then "maybe" else state.submode;
+      rename = if state.submode == "renamed" || state.submode == "maybe_renamed" then state.acc_rename else "";
+    ''
+    # nix
+    ''
+      (if state.submode == "renamed" || state.submode == "maybe_renamed" then {
+        acc_rename = "''${state.acc_rename}''${cur}";
+      } else {
+        acc_str = "''${state.acc_str}''${cur}";
+        submode = if state.submode == "" then "str" else state.submode;
+      })
     ''
     # nix
     ''
@@ -70,8 +106,11 @@
     ''
     # nix
     ''
-      selectors: obj: let
-        peek = i: if builtins.length selectors == i then { type = "end"; } else builtins.elemAt selectors i;
+      getKey = selector:
+        if selector.rename or "" != "" then selector.rename else selector.value;
+      applySelectors =
+        selectors: obj: let
+          peek = i: if builtins.length selectors == i then { type = "end"; } else builtins.elemAt selectors i;
     ''
     # nix
     ''
@@ -114,7 +153,11 @@
     # nix
     "if selector.type == \"maybe\" || selector.type == \"maybe_\" then"
     # nix
-    "(r: if selector.type == \"maybe\" then { \${selector.value} = r; } else r) (recurse selectors (idx + 1) (builtins.getAttr selector.value obj))"
+    "name = getKey x;"
+    # nix
+    "(r: if selector.type == \"maybe\" then { \${getKey selector} = r; } else r) (recurse selectors (idx + 1) (builtins.getAttr selector.value obj))"
+    # nix
+    "\${getKey sel}"
   ] (builtins.readFile sources.nix-select));
   nix-select = import patched;
 in {
